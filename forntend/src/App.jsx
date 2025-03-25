d420e9cbd5f3293d834a6c99cff196b839b4d1b7
kaziabulhasib <hasibkazi420@gmail.com>
2025-03-25 12:19:47 +0530

    file setupp

---
+++ forntend/src/App.jsx
@@ -0,0 +1,12 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-2xl text-center'>welcome </h1>
+      <Todo/>
+    </div>
+  );
+}
+
+export default App;
